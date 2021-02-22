6b5f6f16ebaef864e4c0317d8eeeb0f904750e0d
Andrew <sir1andrew@list.ru>
2021-02-23 01:05:45 +0300

    menu update

--- StudentHelperMobile/app/src/main/java/com/example/studenthelpermobile/MainMenu.java
+++ StudentHelperMobile/app/src/main/java/com/example/studenthelpermobile/MainMenu.java
@@ -1,6 +1,8 @@
 package com.example.studenthelpermobile;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import static com.example.studenthelpermobile.MainActivity.isStudent;
 import static com.example.studenthelpermobile.MainActivity.login;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity implements View.OnClickListener {
 
     TextView WelcomeText;
+    Button ScheduleToday;
+    Button ScheduleTomorrow;
+    Button ScheduleTwoWeeks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,12 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.main_menu_activity);
 
         WelcomeText = findViewById(R.id.welcome_text);
+        ScheduleToday = findViewById(R.id.schedule_today);
+        ScheduleToday.setOnClickListener(this);
+        ScheduleTomorrow = findViewById(R.id.schedule_tomorrow);
+        ScheduleTomorrow.setOnClickListener(this);
+        ScheduleTwoWeeks = findViewById(R.id.schedule_two_weeks);
+        ScheduleTwoWeeks.setOnClickListener(this);
 
         if(isStudent){
             String s = login + " " + getString(R.string.Group);
@@ -27,5 +38,20 @@ public class MainMenu extends AppCompatActivity {
             WelcomeText.setText(login);
         }
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.schedule_today:
+
+                break;
+            case R.id.schedule_tomorrow:
+
+                break;
+            case R.id.schedule_two_weeks:
+
+                break;
+        }
     }
 }
