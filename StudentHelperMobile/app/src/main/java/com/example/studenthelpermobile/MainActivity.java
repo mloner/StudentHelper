4b087b415f211f613e781b8822b5ad1fd5f9b0ad
Andrew <sir1andrew@list.ru>
2021-02-22 20:35:28 +0300

    login page update

--- StudentHelperMobile/app/src/main/java/com/example/studenthelpermobile/MainActivity.java
+++ StudentHelperMobile/app/src/main/java/com/example/studenthelpermobile/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.studenthelpermobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,26 +11,29 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button Student;
-    Button Prepod;
+    Button StudentBtn;
+    Button PrepodBtn;
     TextView LoginText;
-    EditText Login;
-    TextView EnterPasswordText;
-    EditText Password;
+    EditText LoginField;
+    TextView PasswordText;
+    EditText PasswordField;
     Button LoginButton;
+    TextView ErrorText;
+    boolean isStudent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LoginText = findViewById(R.id.text_enter_fio);
-        EnterPasswordText = findViewById(R.id.text_enter_password);
-        Login = findViewById(R.id.enter_fio);
-        Password = findViewById(R.id.enter_password);
-        Student = findViewById(R.id.choose_student);
-        Student.setOnClickListener(this);
-        Prepod = findViewById(R.id.choose_prepod);
-        Prepod.setOnClickListener(this);
+        PasswordText = findViewById(R.id.text_enter_password);
+        LoginField = findViewById(R.id.enter_fio);
+        ErrorText = findViewById(R.id.error_text);
+        PasswordField = findViewById(R.id.enter_password);
+        StudentBtn = findViewById(R.id.choose_student);
+        StudentBtn.setOnClickListener(this);
+        PrepodBtn = findViewById(R.id.choose_prepod);
+        PrepodBtn.setOnClickListener(this);
         LoginButton = findViewById(R.id.login_button);
         LoginButton.setOnClickListener(this);
     }
@@ -40,18 +44,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.choose_student:
                 LoginText.setVisibility(View.VISIBLE );
                 LoginText.setText(R.string.Enter_Group_number);
-                Login.setVisibility(View.VISIBLE);
-                EnterPasswordText.setVisibility(View.GONE);
-                Password.setVisibility(View.GONE);
+                LoginField.setVisibility(View.VISIBLE);
+                PasswordText.setVisibility(View.GONE);
+                PasswordField.setVisibility(View.GONE);
                 LoginButton.setVisibility(View.VISIBLE);
+                isStudent = true;
                 break;
             case R.id.choose_prepod:
                 LoginText.setVisibility(View.VISIBLE );
                 LoginText.setText(R.string.Enter_FIO);
-                Login.setVisibility(View.VISIBLE );
-                EnterPasswordText.setVisibility(View.VISIBLE);
-                Password.setVisibility(View.VISIBLE);
+                LoginField.setVisibility(View.VISIBLE );
+                PasswordText.setVisibility(View.VISIBLE);
+                PasswordField.setVisibility(View.VISIBLE);
                 LoginButton.setVisibility(View.VISIBLE);
+                isStudent = false;
+                break;
+            case R.id.login_button:
+                String responce;
+                responce = "OK";
+
+                switch (responce){
+                    case "OK":
+                        Intent intent = new Intent(this, MainMenu.class);
+                        startActivity(intent);
+                        break;
+                    case "WRONG_LOGIN":
+                        if(isStudent){
+                            ErrorText.setText(R.string.Group_error);
+                        }
+                        else {
+                            ErrorText.setText(R.string.Login_error);
+                        }
+                        break;
+                    case "WRONG_PASSWORD":
+                        ErrorText.setText(R.string.Password_error);
+                        break;
+                }
+
                 break;
         }
     }
