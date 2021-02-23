dbac6a3c618a443b84066a751576071c73a991e8
Andrew <sir1andrew@list.ru>
2021-02-23 22:11:21 +0300

    prepod info class

---
+++ StudentHelperMobile/app/src/main/java/com/example/studenthelpermobile/PrepodInfo.java
@@ -0,0 +1,31 @@
+package com.example.studenthelpermobile;
+
+import android.os.Bundle;
+
+import com.example.studenthelpermobile.Model.ApiRepository;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PrepodInfo extends AppCompatActivity {
+
+    JSONObject prepodlist;
+    ApiRepository apiRepository;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prepod_info_activity);
+
+        apiRepository = new ApiRepository();
+
+        try {
+            prepodlist = apiRepository.PrepodList();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
