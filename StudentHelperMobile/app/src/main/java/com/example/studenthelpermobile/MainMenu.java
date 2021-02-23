edf05337c5dea33eaae24c3ebae569aaca59c9ed
Andrew <sir1andrew@list.ru>
2021-02-23 21:23:39 +0300

    login and schedule request done

--- StudentHelperMobile/app/src/main/java/com/example/studenthelpermobile/MainMenu.java
+++ StudentHelperMobile/app/src/main/java/com/example/studenthelpermobile/MainMenu.java
@@ -5,6 +5,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.studenthelpermobile.Model.ApiRepository;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import static com.example.studenthelpermobile.MainActivity.isStudent;
@@ -12,10 +17,16 @@ import static com.example.studenthelpermobile.MainActivity.login;
 
 public class MainMenu extends AppCompatActivity implements View.OnClickListener {
 
+    ApiRepository apiRepository;
+
     TextView WelcomeText;
     Button ScheduleToday;
     Button ScheduleTomorrow;
     Button ScheduleTwoWeeks;
+    Button PrepodInfo;
+    Button PrepodChat;
+    Button Logout;
+    JSONObject schedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,12 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
         ScheduleTomorrow.setOnClickListener(this);
         ScheduleTwoWeeks = findViewById(R.id.schedule_two_weeks);
         ScheduleTwoWeeks.setOnClickListener(this);
+        PrepodInfo = findViewById(R.id.prepod_info);
+        PrepodInfo.setOnClickListener(this);
+        PrepodChat = findViewById(R.id.prepod_chat);
+        PrepodChat.setOnClickListener(this);
+        Logout = findViewById(R.id.logout);
+        Logout.setOnClickListener(this);
 
         if(isStudent){
             String s = login + " " + getString(R.string.Group);
@@ -38,20 +55,50 @@ public class MainMenu extends AppCompatActivity implements View.OnClickListener 
             WelcomeText.setText(login);
         }
 
+        apiRepository = new ApiRepository();
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.schedule_today:
-
+                try {
+                    schedule = apiRepository.Schedule(1, login);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                ScheduleUnpacking(schedule);
                 break;
             case R.id.schedule_tomorrow:
-
+                try {
+                    schedule = apiRepository.Schedule(2, login);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                ScheduleUnpacking(schedule);
                 break;
             case R.id.schedule_two_weeks:
+                try {
+                    schedule = apiRepository.Schedule(14, login);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                ScheduleUnpacking(schedule);
+                break;
+            case R.id.prepod_info:
+
+                break;
+            case R.id.prepod_chat:
+
+                break;
+            case R.id.logout:
 
                 break;
         }
+    }
+
+    public void ScheduleUnpacking(JSONObject schedule){
+        //скорее всего в отдельном классе View
     }
 }
