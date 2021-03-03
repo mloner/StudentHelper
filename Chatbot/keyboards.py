9b7e051523cc2f6c6d35e8c2475e034fb6c71591
Oduuvan <satilinvlad@gmail.com>
2021-03-03 23:50:57 +0300

    Обновил бота =)

--- Chatbot/keyboards.py
+++ Chatbot/keyboards.py
@@ -31,7 +31,7 @@ keyboard1 = {
 keyboard1 = json.dumps(keyboard1, ensure_ascii=False).encode('utf-8')
 keyboard1 = str(keyboard1.decode('utf-8'))
 
-# Клава выбор расписания
+# Клава выбор расписания препода
 keyboard2 = {
     "one_time": True,
     "buttons": [
@@ -42,11 +42,12 @@ keyboard2 = {
 keyboard2 = json.dumps(keyboard2, ensure_ascii=False).encode('utf-8')
 keyboard2 = str(keyboard2.decode('utf-8'))
 
-# Пустая
+# Выбора подгруппы
 keyboard3 = {
     "one_time": True,
     "buttons": [
-        [get_but('В начало', 'negative')]
+        [get_but('1 подгруппа', 'primary'), get_but('2 подгруппа', 'primary')],
+        [get_but('Обе подгруппы', 'primary')]
     ]
 }
 keyboard3 = json.dumps(keyboard3, ensure_ascii=False).encode('utf-8')
@@ -61,4 +62,14 @@ keyboard4 = {
 keyboard4 = json.dumps(keyboard4, ensure_ascii=False).encode('utf-8')
 keyboard4 = str(keyboard4.decode('utf-8'))
 
-keyboards = [keyboard0, keyboard1, keyboard2, keyboard3, keyboard4]
+keyboard5 = {
+    "one_time": True,
+    "buttons": [
+        [get_but('На сегодня', 'primary'), get_but('На завтра', 'primary'), get_but('На 2 недели', 'primary')],
+        [get_but('Разлогиниться', 'negative'), get_but('Сменить подгруппу', 'primary')]
+    ]
+}
+keyboard5 = json.dumps(keyboard5, ensure_ascii=False).encode('utf-8')
+keyboard5 = str(keyboard5.decode('utf-8'))
+
+keyboards = [keyboard0, keyboard1, keyboard2, keyboard3, keyboard4, keyboard5]
