31860d1fdbc8c26f740f30a37b5e5d6487f23b28
Oduuvan <satilinvlad@gmail.com>
2021-03-03 21:33:36 +0300

    Бот теперь может выдавать расписания на сегодня и на завтра

--- Chatbot/API_repository.py
+++ Chatbot/API_repository.py
@@ -9,7 +9,9 @@ headers = {'Content-type': 'application/json',  # Определение тип�
            'Accept': 'text/plain',
            'Content-Encoding': 'utf-8'}
 
+# POST
 def _registerUser(idvk:str, role:str, arg:str):
+    url = 'http://shipshon.fvds.ru/api/registerUser'
     req = {}
     req['client_type'] = 'bot'
     req['command'] = 'registerUser'
@@ -21,27 +23,8 @@ def _registerUser(idvk:str, role:str, arg:str):
     print(response)
     return response
 
-def _getAuthData(idvk:str):
-    req = {}
-    req['client_type'] = 'bot'
-    req['command'] = 'getAuthData'
-    req['idvk'] = idvk
-    print(json.dumps(req))
-    response = json.loads(requests.post(url, json.dumps(req), headers=headers).text)
-    print(response)
-    return response
-
-def _getUserState(idvk:str):
-    req = {}
-    req['client_type'] = 'bot'
-    req['command'] = 'getUserState'
-    req['idvk'] = idvk
-    print(json.dumps(req))
-    response = json.loads(requests.post(url, json.dumps(req), headers=headers).text)
-    print(response)
-    return response
-
 def _setUserState(idvk:str, state:str):
+    url = 'http://shipshon.fvds.ru/api/setUserState'
     req = {}
     req['client_type'] = 'bot'
     req['command'] = 'setUserState'
@@ -52,23 +35,62 @@ def _setUserState(idvk:str, state:str):
     print(response)
     return response
 
+def _getUserState(idvk:str):
+    url = 'http://shipshon.fvds.ru/api/getUserState'
+    req = {}
+    req['client_type'] = 'bot'
+    req['command'] = 'getUserState'
+    req['idvk'] = idvk
+    print(json.dumps(req))
+    response = json.loads(requests.post(url, json.dumps(req), headers=headers).text)
+    print(response)
+    return response
+
+# GET
 def _checkGroup(group:str):
+    url = 'http://shipshon.fvds.ru/api/checkGroup'
     req = {}
     req['client_type'] = 'bot'
     req['command'] = 'checkGroup'
     req['group'] = group
     print(json.dumps(req))
-    response = json.loads(requests.post(url, json.dumps(req), headers=headers).text)
+    response = json.loads(requests.get(url, params = req, headers=headers).text)
     print(response)
     return response
 
-def _checkFIO(FIO:str):
+def _checkFio(fio:str):
+    url = 'http://shipshon.fvds.ru/api/checkFio'
     req = {}
     req['client_type'] = 'bot'
-    req['command'] = 'checkFIO'
-    req['FIO'] = FIO
+    req['command'] = 'checkFio'
+    req['fio'] = fio
     print(json.dumps(req))
-    response = json.loads(requests.post(url, json.dumps(req), headers=headers).text)
+    response = json.loads(requests.get(url, params = req, headers=headers).text)
     print(response)
     return response
 
+def _getScheduleStudent(role:str, group:str, schedule_type:str):
+    url = 'http://shipshon.fvds.ru/api/getScheduleStudent'
+    req = {}
+    req['client_type'] = 'bot'
+    req['command'] = 'getScheduleStudent'
+    req['role'] = role
+    req['group'] = group
+    req['schedule_type'] = schedule_type
+    print(json.dumps(req))
+    response = json.loads(requests.get(url, params = req, headers=headers).text)
+    print(response)
+    return response
+
+def _getSchedulePrepod(role:str, fio:str, schedule_type:str):
+    url = 'http://shipshon.fvds.ru/api/getSchedulePrepod'
+    req = {}
+    req['client_type'] = 'bot'
+    req['command'] = 'getSchedulePrepod'
+    req['role'] = role
+    req['fio'] = fio
+    req['schedule_type'] = schedule_type
+    print(json.dumps(req))
+    response = json.loads(requests.get(url, params = req, headers=headers).text)
+    print(response)
+    return response
--- Chatbot/main.py
+++ Chatbot/main.py
@@ -1,6 +1,5 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-import json
 import keyboards
 import API_repository
 
@@ -38,14 +37,7 @@ for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
         log(event)
 
-
         user_state = API_repository._getUserState(event.user_id)['response']
-
-        # authorization = API_repository._getAuthData(event.user_id)
-        # if authorization['status'] == 'FAIL':
-        #     sender(event.user_id, 'Пожалуйтса авторизуйтесь', 4)
-        # else:
-        #     sender(event.user_id, 'Выберите расписание', 2)
 
         if event.text == 'Авторизация' and user_state['state'] != 'Finish':
             API_repository._setUserState(event.user_id, 'RegisterUser')
@@ -53,7 +45,7 @@ for event in longpoll.listen():
 
         elif event.text == 'Студент' and user_state['state'] != 'Finish':
             API_repository._setUserState(event.user_id, 'RegisterUser_S')
-            sender_without_keyboard(event.user_id, 'Введите группу')
+            sender(event.user_id, 'Выберите группу', 1)
 
         elif event.text == 'Преподаватель' and user_state['state'] != 'Finish':
             API_repository._setUserState(event.user_id, 'RegisterUser_P')
@@ -62,6 +54,78 @@ for event in longpoll.listen():
         elif event.text == 'Разлогиниться':
             API_repository._setUserState(event.user_id, 'Start')
             sender(event.user_id, 'Пожалуйтса авторизуйтесь', 4)
+
+        elif event.text == 'На сегодня' and user_state['state'] == 'Finish':
+            if user_state['role'] == 'student':
+                schedule_today = API_repository._getScheduleStudent(user_state['role'], user_state['arg'], 'today')['response']
+
+                schedule_text = '👥 ' + user_state['arg'] + '\n\n'
+                for i in schedule_today:
+                    schedule_text = schedule_text + '📖 ' + i['subjectName'] + '\n'
+                    schedule_text = schedule_text + '📄 ' + i['lessonType'] + '\n'
+                    schedule_text = schedule_text + '⏰ ' + i['lessonStart'] + ' - ' + i['lessonEnd'] + '\n'
+                    if i['subGroup'] == 0:
+                        schedule_text = schedule_text + '🗿 ' + 'Обе подргуппы' + '\n'
+                    elif i['subGroup'] == 1:
+                        schedule_text = schedule_text + '🗿 ' + '1 подргуппа' + '\n'
+                    elif i['subGroup'] == 2:
+                        schedule_text = schedule_text + '🗿 ' + '2 подргуппа' + '\n'
+                    schedule_text = schedule_text + '🚪 ' + i['className'] + '\n'
+                    schedule_text = schedule_text + '🤡 ' + i['prepodName'] + '\n\n'
+
+                sender(event.user_id, schedule_text, 2)
+
+
+            elif user_state['role'] == 'prepod':
+                schedule_today = API_repository._getSchedulePrepod(user_state['role'], user_state['arg'], 'today')
+
+        elif event.text == 'На завтра' and user_state['state'] == 'Finish':
+            if user_state['role'] == 'student':
+                schedule_tomorrow = API_repository._getScheduleStudent(user_state['role'], user_state['arg'], 'tomorrow')['response']
+
+                schedule_text = '👥 ' + user_state['arg'] + '\n\n'
+                for i in schedule_tomorrow:
+                    schedule_text = schedule_text + '📖 ' + i['subjectName'] + '\n'
+                    schedule_text = schedule_text + '📄 ' + i['lessonType'] + '\n'
+                    schedule_text = schedule_text + '⏰ ' + i['lessonStart'] + ' - ' + i['lessonEnd'] + '\n'
+                    if i['subGroup'] == 0:
+                        schedule_text = schedule_text + '🗿 ' + 'Обе подргуппы' + '\n'
+                    elif i['subGroup'] == 1:
+                        schedule_text = schedule_text + '🗿 ' + '1 подргуппа' + '\n'
+                    elif i['subGroup'] == 2:
+                        schedule_text = schedule_text + '🗿 ' + '2 подргуппа' + '\n'
+                    schedule_text = schedule_text + '🚪 ' + i['className'] + '\n'
+                    schedule_text = schedule_text + '🤡 ' + i['prepodName'] + '\n\n'
+
+                sender(event.user_id, schedule_text, 2)
+                # schedule_text = '👥 ' + user_state['arg'] + '\n\n'
+                # for i in schedule_tomorrow:
+                #     schedule_text = schedule_text + '📖 ' + i['lessonType'] + ' '
+                #     schedule_text = schedule_text + i['subjectName'] + '\n'
+                #     schedule_text = schedule_text + '⏰ ' + i['lessonStart'] + ' - ' + i['lessonEnd'] + ' '
+                #     schedule_text = schedule_text + '🚪 ' + i['className'] + '\n'
+                #     if i['subGroup'] == 0:
+                #         schedule_text = schedule_text + '🗿 ' + 'Обе подргуппы' + ' '
+                #     elif i['subGroup'] == 1:
+                #         schedule_text = schedule_text + '🗿 ' + '1 подргуппа' + ' '
+                #     elif i['subGroup'] == 2:
+                #         schedule_text = schedule_text + '🗿 ' + '2 подргуппа' + ' '
+                #     schedule_text = schedule_text + '🤡 ' + i['prepodName'] + '\n\n'
+
+
+
+
+
+            elif user_state['role'] == 'prepod':
+                schedule_tomorrow = API_repository._getSchedulePrepod(user_state['role'], user_state['arg'], 'tomorrow')
+
+        elif event.text == 'На 2 недели' and user_state['state'] == 'Finish':
+            if user_state['role'] == 'student':
+                schedule_two_weeks = API_repository._getScheduleStudent(user_state['role'], user_state['arg'], 'two_weeks')
+
+            elif user_state['role'] == 'prepod':
+                schedule_two_weeks = API_repository._getSchedulePrepod(user_state['role'], user_state['arg'], 'two_weeks')
+
 
 
         #Ввел что-то не из перечня
@@ -92,7 +156,7 @@ for event in longpoll.listen():
                     sender(event.user_id, 'Пожалуйтса авторизуйтесь', 4)
 
             elif user_state['state'] == 'RegisterUser_P':
-                if API_repository._checkFIO(event.text)['status'] == 'OK':
+                if API_repository._checkFio(event.text)['status'] == 'OK':
                     API_repository._registerUser(event.user_id, 'prepod', event.text)
                     sender(event.user_id, 'Вы успешно авторизовались!\n\nВыберите расписание', 2)
                     API_repository._setUserState(event.user_id, 'Finish')
